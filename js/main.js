c4f3f188678a847626f37d63bcda216777378548
shrfrn <shrfrn@gmail.com>
2023-01-18 20:22:24 +0200

    Spacing, semicolons, etc...

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
+'use strict'
+
 // TODO: render the balloons + onload="init()"  
 // TODO: onmouseover="speedUp
 // TODO: createBalloons(count) , createBalloon()
-// TODO: remove balloon1, balloon2 classes - move into inline style="bgcolor,left
+// TODO: remove balloon1, balloon2 classes - move into inline style="bgcolor, left"
 
 //model:
 var gBalloons = [
@@ -9,57 +11,51 @@ var gBalloons = [
     { id: 2, speed: 15, bottom: 0 },
     { id: 3, speed: 15, bottom: 0 }
 ]
-var gInterval;
+var gInterval 
 
 function start() {
-    gInterval = setInterval(moveBalloons, 1000);
+    gInterval = setInterval(moveBalloons, 1000)
 }
 
 function moveBalloons() {
-    var elBalloons = document.querySelectorAll('.balloon');
+    var elBalloons = document.querySelectorAll('.balloon')
 
     for (var i = 0; i < gBalloons.length; i++) {
-        //model
-        var balloon = gBalloons[i];
-        //dom
-        var elBalloon = elBalloons[i];
-        //update the model
-        balloon.bottom += balloon.speed;
-        //update the dom
-        elBalloon.style.bottom = balloon.bottom + 'px';
+        // model
+        var balloon = gBalloons[i]
+        // dom
+        var elBalloon = elBalloons[i]
+
+        // update the model
+        balloon.bottom += balloon.speed
+        // update the dom
+        elBalloon.style.bottom = balloon.bottom + 'px'
     }
 }
 
 function blowingUp(elBalloon) {
-    elBalloon.style.display = 'none';
-    playSound();
+    elBalloon.style.display = 'none'
+    playSound()
 }
 
 function playSound() {
-    var sound = new Audio("sound/pop.mp3");
-    sound.play();
+    var sound = new Audio("sound/pop.mp3")
+    sound.play()
 }
 
-
-
-
-
-
-
-
-
+// Utility Functions
 
 function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min)) + min  // The maximum is exclusive and the minimum is inclusive
 }
 
 function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
+    var letters = '0123456789ABCDEF'
+    var color = '#'
     for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
+        color += letters[Math.floor(Math.random() * 16)]
     }
-    return color;
+    return color
 }
